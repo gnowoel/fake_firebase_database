4e142fbfb2719f7ddc444364162786606bd13492
Leo Wong <gnowoel@gmail.com>
2024-09-29 11:38:31 +0800

    Add all the fake classes

--- lib/fake_firebase_database.dart
+++ lib/fake_firebase_database.dart
@@ -1,3 +1,8 @@
 library fake_firebase_database;
 
+part 'src/fake_data_snapshot.dart';
+part 'src/fake_database_event.dart';
+part 'src/fake_database_reference.dart';
 part 'src/fake_firebase_database.dart';
+part 'src/fake_on_disconnect.dart';
+part 'src/fake_query.dart';
---
+++ lib/src/fake_data_snapshot.dart
@@ -0,0 +1,3 @@
+part of '../fake_firebase_database.dart';
+
+class FakeDataSnapshot {}
