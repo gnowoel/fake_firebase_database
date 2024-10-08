26b39c8efadd42d2b0dbe20842849161e3542e5f
Leo Wong <gnowoel@gmail.com>
2024-10-08 10:33:35 +0800

    Add more tests for the `FakeDataSnapshot.value` getter

--- test/fake_data_snapshot_test.dart
+++ test/fake_data_snapshot_test.dart
@@ -19,6 +19,40 @@ void main() {
 
         expect(snapshot.value, value);
       });
+
+      test('can be null', () {
+        final ref = database.ref('users/123');
+        final snapshot = FakeDataSnapshot(ref, null);
+
+        expect(snapshot.value, null);
+      });
+
+      test('can be a primitive type', () {
+        final ref = database.ref('users/123/name');
+        const value = 'John';
+        final snapshot = FakeDataSnapshot(ref, value);
+
+        expect(snapshot.value, value);
+      });
+
+      test('value can be a complex nested structure', () {
+        final ref = database.ref('users');
+        final value = {
+          '123': {
+            'name': 'John',
+            'age': 30,
+            'address': {'city': 'New York', 'country': 'USA'}
+          },
+          '456': {
+            'name': 'Jane',
+            'age': 25,
+            'address': {'city': 'London', 'country': 'UK'}
+          }
+        };
+        final snapshot = FakeDataSnapshot(ref, value);
+
+        expect(snapshot.value, value);
+      });
     });
   });
 }
