9425b97d8d0a6d8b690846fee39a28581250d9ed
Leo Wong <gnowoel@gmail.com>
2024-10-26 17:22:35 +0800

    Ensure `ServerValue` works on a deep path

--- test/server_value_test.dart
+++ test/server_value_test.dart
@@ -19,11 +19,30 @@ void main() {
         expect(snapshot.value, isA<num>());
         expect(snapshot.value, isNonZero);
       });
+
+      test('can set current timestamp on a deep path', () async {
+        final ref = database.ref('a/b/c');
+        await ref.set(ServerValue.timestamp);
+
+        final snapshot = await ref.get();
+        expect(snapshot.value, isA<num>());
+        expect(snapshot.value, isNonZero);
+      });
     });
 
-    group('increment', () {
+    group('increment()', () {
       test('can increment numbers', () async {
         final ref = database.ref();
+        await ref.set(4);
+
+        await ref.set(ServerValue.increment(3));
+
+        final snapshot = await ref.get();
+        expect(snapshot.value, 7);
+      });
+
+      test('can increment numbers on a deep path', () async {
+        final ref = database.ref('a/b/c');
         await ref.set(4);
 
         await ref.set(ServerValue.increment(3));
